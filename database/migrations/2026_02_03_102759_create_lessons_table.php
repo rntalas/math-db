93fea8364915b32788f6a9cd96f948c2e0060bae
rntalas <rafailntalas@outlook.com>
2026-02-03 12:34:53 +0200

    feat: create migrations for subjects, lessons, entries

---
+++ database/migrations/2026_02_03_102759_create_lessons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+
+            $table->foreignId('subject_id')
+                ->nullable()
+                ->constrained('subjects')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
---
+++ database/migrations/2026_02_03_102800_create_entries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->string('image')->nullable();
+            $table->text('solution')->nullable();
+
+            $table->foreignId('lesson_id')
+                ->nullable()
+                ->constrained('lessons')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+};
