1558c7c057b2d818b55d2551989308172c0972ca
rntalas <rafailntalas@outlook.com>
2026-02-04 02:09:49 +0200

    feat: create localSwitcher and inputLocalSwitcher functions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,45 @@
 import './bootstrap';
 import "../css/app.css";
 
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
 
-window.Alpine = Alpine
-Alpine.start()
+window.Alpine = Alpine;
+
+Alpine.data('localeSwitcher', () => ({
+    open: false,
+    locales: [
+        { code: 'en', label: 'English' },
+        { code: 'el', label: 'Ελληνικά' }
+    ],
+    toggle() {
+        this.open = !this.open
+    },
+    setLocale(code) {
+        window.location.href = `${window.location.pathname}?lang=${code}`
+    }
+}));
+
+Alpine.data('inputLocaleSwitcher', (Locales) => {
+    const localesData = Locales.map(locale => ({
+        id: locale.id,
+        name: locale.name,
+        image: locale.image.startsWith('http') ? locale.image : `/${locale.image}`
+    }));
+
+    return {
+        open: false,
+        localesData,
+        selectedLocale: localesData[0],
+        titles: {},
+        touched: {},
+
+        selectLocale(locale) {
+            this.selectedLocale = locale;
+            this.open = false;
+        }
+    }
+});
+
+
+
+Alpine.start();
