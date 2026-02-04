3788a7f2a9a38a543c23b3f287fe97761ade4748
rntalas <rafailntalas@outlook.com>
2026-02-04 16:49:30 +0200

    refactor: update language files

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'welcome' => 'Welcome to <a href="/" class="font-bold hover:underline">Answer Pod</a>!</span>',
+    'welcome' => 'Welcome to <a href="/" class="font-bold hover:underline">Answer Pod</a>!',
     'description' => 'The best place to store textbook materials for your school subjects.',
     'subjects' => [
         'found' => 'The following subjects were found:',
         '!found' => 'No subjects were found.',
         'create' => 'To create a subject click <a href="subjects/create" class="link">here</a>.',
     ],
+    'search' => 'Search...',
 ];
