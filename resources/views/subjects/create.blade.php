5d55d3b55cf026dbccec9fcdff8c35aab0542bbc
rntalas <rafailntalas@outlook.com>
2026-02-05 01:16:36 +0200

    feat(subjects): add create

---
+++ resources/views/subjects/create.blade.php
@@ -0,0 +1,27 @@
+<x-layout :title="__('subject.create')">
+    <h1 class="font-bold text-xl mb-4">@lang('subject.create')</h1>
+    <form action="{{ route('subjects.create') }}" method="POST" class="flex flex-col gap-4">
+        @csrf
+        <div class="flex flex-col gap-4 mb-4">
+            <div class="flex flex-col gap-1">
+                <label for="name" class="text-lg font-bold">@lang('subject.label.name')</label>
+                <input type="text" id="name" name="name" placeholder="@lang('subject.placeholder.name')"
+                    value="{{ old('name') }}" class="rounded-xl w-full px-3 py-2">
+
+                @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="flex flex-col gap-2">
+                <label for="units" class="text-lg font-bold">@lang('subject.label.units')</label>
+                <input type="number" id="units" name="units" min="1" max="100"
+                    value="{{ old('units', 1) }}" class="rounded-xl w-fit">
+            </div>
+
+            <input type="hidden" name="locale_id" value="1">
+
+            <input type="submit" value="Add Subject" class="btn mt-4">
+        </div>
+    </form>
+</x-layout>
