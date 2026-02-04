fb8e6f6bfcbcfc51f84df00c6ce15db10927c9fd
rntalas <rafailntalas@outlook.com>
2026-02-05 01:26:36 +0200

    feat(subject): update view.blade.php

---
+++ resources/views/subjects/view.blade.php
@@ -0,0 +1,21 @@
+<x-layout :title="$subject->name">
+    <div class="flex items-center gap-2 pb-4">
+        <h1 class="text-xl font-bold">{{ $subject->name }}</h1>
+
+        <a href="{{ route('subjects.edit', $subject->id) }}" title="@lang('app.edit')">
+            @svg('heroicon-s-pencil-square', 'h-5 w-5 cursor-pointer')
+        </a>
+
+        <x-confirm-delete :action="route('subjects.destroy', $subject->id)" :title="__('subject.confirm')">
+            @svg('heroicon-s-x-mark', 'h-5 w-5 cursor-pointer text-red-600')
+        </x-confirm-delete>
+    </div>
+
+    <p class="pb-4">
+        @lang('subject.units', ['count' => $subject->units])
+    </p>
+
+    <p class="pb-4">
+        @lang('subject.entries')
+    </p>
+</x-layout>
