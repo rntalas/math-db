cecbbf3bc49dd6bd3c9469858c53c2e6bd778bcb
rntalas <rafailntalas@outlook.com>
2026-02-04 12:30:59 +0200

    chore: update LocalesTableSeeder

--- database/seeders/LocalesTableSeeder.php
+++ database/seeders/LocalesTableSeeder.php
@@ -10,7 +10,7 @@ class LocalesTableSeeder extends Seeder
     public function run():void {
         DB::table('locales')->insert([
            ['name' => 'English', 'code' => 'en', 'image' => 'images/lang/en.png', 'created_at' => now(), 'updated_at' => now()],
-           ['name' => 'Greek', 'code' => 'el', 'image' => 'images/lang/el.png', 'created_at' => now(), 'updated_at' => now()],
+           ['name' => 'Ελληνικά', 'code' => 'el', 'image' => 'images/lang/el.png', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
