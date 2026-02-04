31cd9ee487f2ea4b97471798b714276ac240316b
rntalas <rafailntalas@outlook.com>
2026-02-04 02:02:47 +0200

    feat: create language files for greek and english

---
+++ resources/lang/el/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => 'Καλώς ήρθατε στο <a href="/" class="font-bold hover:underline">Answer Pod</a>!',
+    'description' => 'Το καλύτερο μέρος για να αποθηκεύσετε υλικό από τα σχολικά σας βιβλία.',
+    'subjects' => [
+        'found' => 'Βρέθηκαν τα ακόλουθα μαθήματα:',
+        '!found' => 'Δεν βρέθηκαν μαθήματα.',
+        'create' => 'Για να δημιουργήσετε ένα μάθημα, κάντε κλικ <a href="subjects/create" class="link">εδώ</a>.',
+    ],
+];
---
+++ resources/lang/en/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to <a href="/" class="font-bold hover:underline">Answer Pod</a>!</span>',
+    'description' => 'The best place to store textbook materials for your school subjects.',
+    'subjects' => [
+        'found' => 'The following subjects were found:',
+        '!found' => 'No subjects were found.',
+        'create' => 'To create a subject click <a href="subjects/create" class="link">here</a>.',
+    ],
+];
