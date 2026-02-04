2426790d2b55c4a8068d94ee631bc863d4f69de8
rntalas <rafailntalas@outlook.com>
2026-02-05 01:29:37 +0200

    feat: update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,13 @@
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SubjectController;
 
-Route::resource('subjects', SubjectController::class);
+Route::prefix('subject')->group(function () {
+    Route::get('add', [SubjectController::class, 'create'])->name('subjects.create');
+    Route::post('add', [SubjectController::class, 'store'])->name('subjects.store');
+    Route::get('{id}', [SubjectController::class, 'show'])->name('subjects.show');
+    Route::get('edit/{id}', [SubjectController::class, 'edit'])->name('subjects.edit');
+    Route::delete('delete/{id}', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+});
 
 Route::get('/{slug?}', [PageController::class, 'index'])
     ->where('slug', '.*')
