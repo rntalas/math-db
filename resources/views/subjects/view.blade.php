68782ccca8c1175f3f72b988a4ea2eb2bcfc06e2
rntalas <rafailntalas@outlook.com>
2026-02-05 11:00:39 +0200

    feat(subjects/view): update routes

--- resources/views/subjects/view.blade.php
+++ resources/views/subjects/view.blade.php
@@ -2,11 +2,11 @@
     <div class="flex items-center gap-2 pb-4">
         <h1 class="text-xl font-bold">{{ $subject->name }}</h1>
 
-        <a href="{{ route('subjects.edit', $subject->id) }}" title="@lang('app.edit')">
+        <a href="{{ route('subject.edit', $subject) }}" title="@lang('app.edit')">
             @svg('heroicon-s-pencil-square', 'h-5 w-5 cursor-pointer')
         </a>
 
-        <x-confirm-delete :action="route('subjects.destroy', $subject->id)" :title="__('subject.confirm')">
+        <x-confirm-delete :action="route('subject.destroy', $subject)" :title="__('subject.confirm')">
             @svg('heroicon-s-x-mark', 'h-5 w-5 cursor-pointer text-red-600')
         </x-confirm-delete>
     </div>
