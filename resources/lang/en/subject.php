dd59418ac7e575d8f13e656742187427ca42ef69
rntalas <rafailntalas@outlook.com>
2026-02-05 19:19:31 +0200

    chore(xx/subject): update lang files

--- resources/lang/en/subject.php
+++ resources/lang/en/subject.php
@@ -3,7 +3,7 @@
 return [
     'create' => 'Create a subject',
     'units' => 'Number of units: :count.',
-    'entries' => 'You can see the entries per unit on the side menu. Otherwise, to create a new entry you can click <a href="entries/create" class="link">here</a>.',
+    'entries' => 'You can see the entries per unit on the side menu. Otherwise, to create a new entry you can click <a href="/entry/create" class="link">here</a>.',
     'button' => [
         'add' => 'Add subject',
         'save' => 'Save changes',
