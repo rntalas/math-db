66403f2790bc5e7ac297df92a76964b2af49ee67
rntalas <rafailntalas@outlook.com>
2026-02-05 11:01:19 +0200

    feat(routes/web.php): simplify subject related routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SubjectController;
 
-Route::prefix('subject')->group(function () {
-    Route::get('add', [SubjectController::class, 'create'])->name('subjects.create');
-    Route::post('add', [SubjectController::class, 'store'])->name('subjects.store');
-    Route::get('{id}', [SubjectController::class, 'show'])->name('subjects.show');
-    Route::get('edit/{id}', [SubjectController::class, 'edit'])->name('subjects.edit');
-    Route::delete('delete/{id}', [SubjectController::class, 'destroy'])->name('subjects.destroy');
-});
+Route::resource('subject', SubjectController::class);
 
 Route::get('/{slug?}', [PageController::class, 'index'])
     ->where('slug', '.*')
