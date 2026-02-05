4db2b2f761895f9d35ca1bdaf6e73686331eb190
rntalas <rafailntalas@outlook.com>
2026-02-05 13:16:36 +0200

    feat(ViewServiceProvider): add view with current locale

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Locale;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,6 +11,17 @@ class ViewServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        View::composer('*', static function ($view) {
+            $currentLocaleCode = App::getLocale();
+            $currentLocaleId = cache()->remember(
+                "locale_id_{$currentLocaleCode}",
+                3600,
+                fn () => Locale::query()->where('code', $currentLocaleCode)->value('id') ?? config('app.default_locale_id', 1)
+            );
+
+            $view->with('currentLocaleId', $currentLocaleId);
+        });
+
         View::composer('partials.header', static function ($view) {
             $view->with(
                 'locales',
