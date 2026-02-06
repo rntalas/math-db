d7e23daddbb8abe75252a0e91065b12436732515
rntalas <rafailntalas@outlook.com>
2026-02-06 03:36:24 +0200

    feat(entries/create): format file & reorder options

--- resources/views/entries/create.blade.php
+++ resources/views/entries/create.blade.php
@@ -5,7 +5,6 @@
     <form action="{{ route('entry.store') }}" method="POST" class="flex flex-col gap-4">
         @csrf
         <div class="flex flex-col gap-4 mb-4">
-
             <div x-data="{ subjects: @js($subjects), subject: null }" class="flex flex-col gap-4">
                 <div class="flex flex-col gap-2">
                     <label for="subject_id">@lang('entry.label.subject')</label>
@@ -30,82 +29,76 @@
                 <div class="flex flex-col gap-2" x-show="subject?.units" x-cloak>
                     <label for="number">@lang('entry.label.number')</label>
                     <input type="number" id="number" name="number" min="1" max="100"
-                           value="{{ old('number', 1) }}" class="w-fit">
-                </div>
-            </div>
-
-            <div x-data="{ option: 1 }" class="flex flex-col gap-4">
-                <div class="flex gap-4">
-                    <label>@lang('entry.label.statement')</label>
-
-                    <div class="flex gap-2 items-center">
-                        <button type="button" @click="option = 1"
-                            :class="option === 1 ?
-                                'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
-                                'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
-                            @svg('heroicon-o-photo', 'h-4 w-4')
-
-                        </button>
-
-                        <button type="button" @click="option = 2"
-                            :class="option === 2 ?
-                                'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
-                                'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
-                            @svg('heroicon-o-pencil-square', 'h-4 w-4')
-                        </button>
-                    </div>
+                        value="{{ old('number', 1) }}" class="w-fit">
                 </div>
 
-                <div x-show="option === 1">
-                    <x-dropzone-file el="statement" name="statement_file"></x-dropzone-file>
-                </div>
+                <div x-data="{ option: 1 }" class="flex flex-col gap-4" x-show="subject?.units" x-cloak>
+                    <div class="flex gap-4">
+                        <label>@lang('entry.label.statement')</label>
 
-                <div x-show="option === 2" x-cloak>
-                    <textarea name="statement_text" :disabled="option !== 2" placeholder="@lang('entry.placeholder.statement')" class="w-full" cols="30"
-                        rows="3"></textarea>
-                </div>
+                        <div class="flex gap-2 items-center">
+                            <button type="button" @click="option = 1"
+                                :class="option === 1 ?
+                                    'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
+                                    'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
+                                @svg('heroicon-o-pencil-square', 'h-4 w-4')
+                            </button>
 
-                <p class="text-gray-500 text-sm">@lang('entry.hint.statement')</p>
-            </div>
-
-            <div x-data="{ option: 1 }" class="flex flex-col gap-4">
-                <div class="flex gap-4">
-                    <label>@lang('entry.label.solution')</label>
-
-                    <div class="flex gap-2 items-center">
-
-
-                        <button type="button" @click="option = 1"
-                            :class="option === 1 ?
-                                'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
-                                'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
-                            @svg('heroicon-o-code-bracket', 'h-4 w-4')
-                        </button>
-
-                        <button type="button" @click="option = 2"
+                            <button type="button" @click="option = 2"
                                 :class="option === 2 ?
-                                'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
-                                'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
-                            @svg('heroicon-o-pencil-square', 'h-4 w-4')
+                                    'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
+                                    'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
+                                @svg('heroicon-o-photo', 'h-4 w-4')
 
-                        </button>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div x-show="option === 1" x-cloak>
+                        <textarea name="statement_text" :disabled="option !== 1" placeholder="@lang('entry.placeholder.statement')" class="w-full" cols="30"
+                            rows="3"></textarea>
+                    </div>
+
+                    <div x-show="option === 2">
+                        <x-dropzone-file el="statement" name="statement_file"></x-dropzone-file>
                     </div>
                 </div>
 
-                <div x-show="option === 1">
-                    <x-code-editor name="solution" label="HTML Code" language="html" height="h-96"
-                                   :value="old('solution', '')" />
+                <div x-data="{ option: 1 }" class="flex flex-col gap-4" x-show="subject?.units" x-cloak>
+                    <div class="flex gap-4">
+                        <label>@lang('entry.label.solution')</label>
+
+                        <div class="flex gap-2 items-center">
+                            <button type="button" @click="option = 1"
+                                :class="option === 1 ?
+                                    'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
+                                    'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
+                                @svg('heroicon-o-pencil-square', 'h-4 w-4')
+
+                            </button>
+
+                            <button type="button" @click="option = 2"
+                                :class="option === 2 ?
+                                    'bg-gray-100 h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center' :
+                                    'bg-white h-6 w-6 border border-zinc-200 p-1 rounded flex justify-center items-center hover:bg-gray-100'">
+                                @svg('heroicon-o-code-bracket', 'h-4 w-4')
+                            </button>
+                        </div>
+                    </div>
+
+                    <div x-show="option === 1" x-cloak>
+                        <textarea name="solution" :disabled="option !== 1" placeholder="@lang('entry.placeholder.solution')" class="w-full" cols="30"
+                            rows="3"></textarea>
+                    </div>
+
+                    <div x-show="option === 2">
+                        <x-code-editor name="solution" label="HTML Code" language="html" height="h-96"
+                            :value="old('solution', '')" />
+                    </div>
                 </div>
 
-                <div x-show="option === 2"  x-cloak>
-                    <textarea name="solution" :disabled="option !== 2" placeholder="@lang('entry.placeholder.solution')" class="w-full" cols="30"
-                        rows="3"></textarea>
-                </div>
-
+                <input type="hidden" name="locale_id" value="1">
             </div>
-
-            <input type="hidden" name="locale_id" value="1">
-
             <input type="submit" value="@lang('entry.button.add')" class="btn mt-4">
         </div>
     </form>
